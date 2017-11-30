0ba77688811efa6d6cf2f9b72c2ba91c3c110ada
Ramon Fontes <ramonreisfontes@gmail.com>
2017-11-30 08:28:47 -0300

    update code

--- mininet/test/test_walkthrough.py
+++ mininet/test/test_walkthrough.py
@@ -421,7 +421,7 @@ class testWalkthrough(unittest.TestCase):
     def testErrorProb(self):
         "Start Mininet-WiFi, then test wmediumd_error_prob.py"
         p = pexpect.spawn(
-            'python examples / wmediumd_error_prob.py')
+            'python examples/wmediumd_error_prob.py')
         sleep(4)
         p.sendline('sta1 ping -c1 sta2')
         p.expect('1 packets transmitted, 1 received')
